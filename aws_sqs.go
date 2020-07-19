7d08492815466ac0c50660b1b3b8b95d8f7e6c31
Tyson Trautmann <tysont@gmail.com>
2020-07-19 14:29:46 -0700

    Adding AWS SQS functionality.

--- aws_sqs.go
+++ aws_sqs.go
@@ -1,1 +1,115 @@
 package desultory
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+const AwsSqsQueuePrefix = "-queue"
+
+func GetAwsSqsQueuePath(queueName string, stack string) (string, error) {
+	return GetAwsResourcePath(queueName, AwsSqsQueuePrefix, stack)
+}
+
+func GetAwsSqsQueueNameFromPath(queueName string, stack string) (string, error) {
+	return GetAwsResourceNameFromPath(queueName, AwsSqsQueuePrefix, stack)
+}
+
+func CreateAwsSqsQueue(sess *session.Session, queueName string, stack string) (string, error) {
+	svc := sqs.New(sess)
+	qp, err := GetAwsSqsQueuePath(queueName, stack)
+	if err != nil {
+		return "", err
+	}
+	ts := make(map[string]*string, 0)
+	ts[awsStackKey] = aws.String(stack)
+	cqi := &sqs.CreateQueueInput{
+		QueueName: aws.String(qp),
+		Tags:     ts,
+	}
+	cqo, err := svc.CreateQueue(cqi)
+	if err != nil {
+		return "", err
+	}
+	as := []*string{aws.String(sqs.QueueAttributeNameQueueArn)}
+	gqai := &sqs.GetQueueAttributesInput{
+		AttributeNames: as,
+		QueueUrl:       cqo.QueueUrl,
+	}
+	gqao, err := svc.GetQueueAttributes(gqai)
+	if err != nil {
+		return "", err
+	}
+	arn := gqao.Attributes[sqs.QueueAttributeNameQueueArn]
+	return *arn, nil
+}
+
+func DeleteAwsSqsQueue(sess *session.Session, queueName string, stack string) error {
+	svc := sqs.New(sess)
+	qp, err := GetAwsSqsQueuePath(queueName, stack)
+	if err != nil {
+		return err
+	}
+	gqui := &sqs.GetQueueUrlInput{
+		QueueName: aws.String(qp),
+	}
+	url, err := svc.GetQueueUrl(gqui)
+	if err != nil {
+		return err
+	}
+	dqi := &sqs.DeleteQueueInput{
+		QueueUrl: url.QueueUrl,
+	}
+	_, err = svc.DeleteQueue(dqi)
+	return err
+}
+
+func SendAwsSqsMessage(sess *session.Session, queueName string, stack string, message string) error {
+	svc := sqs.New(sess)
+	qp, err := GetAwsSqsQueuePath(queueName, stack)
+	if err != nil {
+		return err
+	}
+	gqui := &sqs.GetQueueUrlInput{
+		QueueName: aws.String(qp),
+	}
+	url, err := svc.GetQueueUrl(gqui)
+	if err != nil || url == nil {
+		return err
+	}
+	smi := &sqs.SendMessageInput{
+		QueueUrl: url.QueueUrl,
+		MessageBody: aws.String(message),
+	}
+	_, err = svc.SendMessage(smi)
+	return err
+}
+
+func ReceiveAwsSqsMessages(sess *session.Session, queueName string, stack string, waitSeconds int) ([]string, error) {
+	svc := sqs.New(sess)
+	qp, err := GetAwsSqsQueuePath(queueName, stack)
+	if err != nil {
+		return nil, err
+	}
+	gqui := &sqs.GetQueueUrlInput{
+		QueueName: aws.String(qp),
+	}
+	url, err := svc.GetQueueUrl(gqui)
+	if err != nil {
+		return nil, err
+	}
+	rmi := &sqs.ReceiveMessageInput{
+		QueueUrl: url.QueueUrl,
+		WaitTimeSeconds: aws.Int64(int64(waitSeconds)),
+	}
+	rmo, err := svc.ReceiveMessage(rmi)
+	if err != nil {
+		return nil, err
+	}
+	ms := make([]string, 0)
+	for _, m := range rmo.Messages {
+		ms = append(ms, *m.Body)
+	}
+	return ms, nil
+}
---
+++ aws_sqs_test.go
@@ -0,0 +1,27 @@
+package desultory
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateSendReceiveDeleteAwsSqsQueue(t *testing.T) {
+	if !getOffline() {
+		assert := assert.New(t)
+		sess, stack, err := setupAwsTest()
+		defer DeleteAwsStack(sess, stack)
+		assert.NoError(err)
+		qn := "test"
+		_, err = CreateAwsSqsQueue(sess, qn, stack)
+		assert.NoError(err)
+		m := "peter piper picked a pack of pickled peppers"
+		err = SendAwsSqsMessage(sess, qn, stack, m)
+		assert.NoError(err)
+		ms, err := ReceiveAwsSqsMessages(sess, qn, stack, 5)
+		assert.NoError(err)
+		assert.Equal(len(ms), 1)
+		assert.Equal(m, ms[0])
+		err = DeleteAwsSqsQueue(sess, qn, stack)
+		assert.NoError(err)
+	}
+}
